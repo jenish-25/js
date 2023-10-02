688c6fa74c7512b582ec97ba6bd5892d055cb5e9
jenishvavdiya <113230851+jenish-25@users.noreply.github.com>
2023-10-02 16:04:19 +0000

    convertionOpes

---
+++ 01_basics/03_convertionOpe.js
@@ -0,0 +1,60 @@
+let score="33abs";
+// let score=true;
+// console.log(typeof score);
+// console.log(typeof (score));
+let valueInNum=Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+let isLoggedIn = "jenish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(13%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);    //122
+// console.log(1 + 2 + "2");    //32
+
+// console.log( (3 + 4) * 5 % 3);  //7*5%3==> 2
+
+// console.log(+true);//1
+// console.log(true);
+// console.log(+"");//0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
