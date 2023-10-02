dd1708febcdd30540d319e6555d85a7230c68d49
jenishvavdiya <113230851+jenish-25@users.noreply.github.com>
2023-10-02 10:57:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert(3+3)   //we are use node js not browser 
+
+// console.log(3+3);
+let name="jenish"
+let age=19
+let isLoggedIn=false
+let state;
+
+//number ==> 2 to power 53
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null=>  standalone value
+// undefined ==>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof name);
+console.log(typeof age);
