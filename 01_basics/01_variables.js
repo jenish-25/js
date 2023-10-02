b342f6801085aa585971ce3de1e2c2fac6d22ad6
jenishvavdiya <113230851+jenish-25@users.noreply.github.com>
2023-10-02 10:33:09 +0000

    variables config

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,12 @@
 const id=2531
 let email="jenish@gmail.com"
 var pass="1234"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 city="surat"
 
 // id=258             //not allowed
