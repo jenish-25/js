69c444bb976f19dae45bc63db4e8604106381282
jenishvavdiya <113230851+jenish-25@users.noreply.github.com>
2023-10-02 09:31:13 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const id=2531
+let email="jenish@gmail.com"
+var pass="1234"
+city="surat"
+
+// id=258             //not allowed
+email="jemspatel@gmmial.com"
+pass=968
+city="banglore"
+console.log(email);
+console.log(id);
+console.table([id,pass,city,email])
